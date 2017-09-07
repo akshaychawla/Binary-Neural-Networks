c11a8f0d71d205bbb5c537be31dfc0dbd3d36ca6
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-09-07 07:22:21 -0400

    remove .pdf file, update cifar_conv_binary with deeper model Something very wrong with cifar binary model; extremely high loss; no increment in val_acc; (TODO): debug binary model
    I have a theory.. maybe I'm not updating the right parameters. Also, check if loss is log(0.1) in the beginning.

--- cifar_conv_binary.py
+++ cifar_conv_binary.py
@@ -18,7 +18,7 @@ import gzip, cPickle, math
 from tensorboard_logging import Logger
 from tqdm import *
 from time import time 
-from layers import Dense, Activation, Dropout, Conv2D, Pool2D, Flatten
+from layers import Dense, Activation, Dropout, Conv2D, Pool2D, Flatten, BinaryConv2D, BinaryDense
 from utils import get_cifar10, ModelCheckpoint, load_model, unpickle
 import ipdb
 import argparse
@@ -35,6 +35,11 @@ def main():
     train_x, train_y, valid_x, valid_y, test_x, test_y = get_cifar10('./cifar-10-batches-py/')
     labels = unpickle('./cifar-10-batches-py/batches.meta')['label_names']
 
+    train_x = train_x.astype(np.float32) / 255.0
+    valid_x = valid_x.astype(np.float32) / 255.0
+    test_x  = test_x.astype(np.float32) / 255.0
+
+
     num_epochs = args.epochs
     eta        = args.lr
     batch_size = args.batch_size
@@ -44,30 +49,35 @@ def main():
     y = T.ivector("y")
     
     # test values
-    # x.tag.test_value = np.random.randn(5, 3, 32, 32)
+    # x.tag.test_value = np.random.randn(6, 3, 32, 32).astype(np.float32)
     # y.tag.test_value = np.array([1,2,1,4,5]).astype(np.int32)
-    
+    # x.tag.test_value = x.tag.test_value / x.tag.test_value.max()
+
+    # import ipdb; ipdb.set_trace()
+
     # network definition 
-    conv1 = BinaryConv2D(input=x, num_filters=16, input_channels=3, size=3, strides=(1,1), padding=1,  name="conv1")
+    conv1 = BinaryConv2D(input=x, num_filters=50, input_channels=3, size=3, strides=(1,1), padding=1,  name="conv1")
     act1  = Activation(input=conv1.output, activation="relu", name="act1")
     pool1 = Pool2D(input=act1.output, stride=(2,2), name="pool1")
     
-    conv2 = BinaryConv2D(input=pool1.output, num_filters=20, input_channels=16, size=3, strides=(1,1), padding=1,  name="conv2")
+    conv2 = BinaryConv2D(input=pool1.output, num_filters=100, input_channels=50, size=3, strides=(1,1), padding=1,  name="conv2")
     act2  = Activation(input=conv2.output, activation="relu", name="act2")
     pool2 = Pool2D(input=act2.output, stride=(2,2), name="pool2")
 
-    conv3 = BinaryConv2D(input=pool2.output, num_filters=32, input_channels=20, size=3, strides=(1,1), padding=1,  name="conv3")
+    conv3 = BinaryConv2D(input=pool2.output, num_filters=200, input_channels=100, size=3, strides=(1,1), padding=1,  name="conv3")
     act3  = Activation(input=conv3.output, activation="relu", name="act3")
     pool3 = Pool2D(input=act3.output, stride=(2,2), name="pool3")
 
     flat  = Flatten(input=pool3.output)
-    fc1   = BinaryDense(input=flat.output, n_in=32*4*4, n_out=10, name="fc1")
-    softmax  = Activation(input=fc1.output, activation="softmax", name="softmax")
+    fc1   = BinaryDense(input=flat.output, n_in=200*4*4, n_out=500, name="fc1")
+    act4  = Activation(input=fc1.output, activation="relu", name="act4")
+    fc2   = BinaryDense(input=act4.output, n_in=500, n_out=10, name="fc2")
+    softmax  = Activation(input=fc2.output, activation="softmax", name="softmax")
 
     # loss
     xent     = T.nnet.nnet.categorical_crossentropy(softmax.output, y)
-    cost     = xent.mean() 
-    
+    cost     = xent.mean()
+
     # errors 
     y_pred   = T.argmax(softmax.output, axis=1)
     errors   = T.mean(T.neq(y, y_pred))
